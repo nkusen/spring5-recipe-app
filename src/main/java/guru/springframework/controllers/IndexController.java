4ac15627df63623a7027a0a4081fca6a58ff8146
Nikola Kušen <nikola.kusen@gmail.com>
2022-08-06 16:34:21 +0200

    Add JPA query methods

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,14 +1,34 @@
 package guru.springframework.controllers;
 
+import guru.springframework.models.Category;
+import guru.springframework.models.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
-        System.out.println("Message");
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByUom("Teaspoon");
+
+        System.out.println("Category ID: " + categoryOptional.get().getId());
+        System.out.println("UnitOfMeasure ID: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
